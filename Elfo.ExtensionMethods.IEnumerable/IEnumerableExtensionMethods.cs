ba313a1628f44a7fb96c6c7314306cea85234bbf
Mirko Bellabarba <mirko.bellabarba@gmail.com>
2018-06-04 18:24:55 +0200

    Initial commit

---
+++ Elfo.ExtensionMethods.IEnumerable/IEnumerableExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Elfo.ExtensionMethods.IEnumerable
+{
+    public static class IEnumerableExtensionMethods
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> list) => (list == null || list?.Count() == 0);
+
+        public static bool ContainsAtLeastOneValue<T>(this IEnumerable<T> list) => list != null && !list.IsNullOrEmpty();
+
+        public static IEnumerable<T> RemoveWhere<T>(this IEnumerable<T> query, Predicate<T> predicate) => query = query.Where(e => !predicate(e));
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Elfo.ExtensionMethods
